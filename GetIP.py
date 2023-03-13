c2259cd9e66ddd650389e3a2e1ced9ab2128c7b2
Alex Moreira <alexlmoreira@outlook.com>
2023-03-13 19:15:53 -0300

    Update GetIP.py

--- GetIP.py
+++ GetIP.py
@@ -4,12 +4,12 @@ import time
 import socket
 import urllib.request
 import requests
-
+wan_ipv6="null"
 # Obtenha o token do bot e substitua o valor abaixo
 def send_to_telegram(message):
-    
-    apiToken = 'SEUTOKEN'
-    chatID = 'SEU-CHAT-ID'
+
+    apiToken = 'SEU_TOKEN'
+    chatID = 'CHAT_ID'
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
 
     try:
@@ -21,40 +21,45 @@ def send_to_telegram(message):
 
 if platform.system() == "Windows":
     ping_command = "ping -n 1 google.com > nul"
+    ping_command6 = "ping -6 -n 1 google.com > nul"
 else:
     ping_command = "ping -c 1 google.com > /dev/null 2>&1"
+    ping_command6 = "ping6 -c 1 google.com > /dev/null 2>&1"
 
 while True:
     response = os.system(ping_command)
+    response6 = os.system(ping_command6)
 
     if response == 0:
         print("Ping concluído com sucesso.")
 
         # Obtém o endereço IP da interface de rede LAN em IPv4 e IPv6
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
+        s.connect(("google.com", 80))
         lan_ipv4 = s.getsockname()[0]
         s.close()
-        #lan_ipv6 = socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6)[0][4][0]
+#       lan_ipv6 = socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6)[0][4][0]
 
 
         hostname = socket.gethostname()
         print("Endereço IP da LAN em IPv4:", lan_ipv4)
-        #print("Endereço IP da LAN em IPv6:", lan_ipv6)
 
         # Obtém o endereço IP da interface de rede WAN em IPv4 e IPv6
         wan_ipv4 = urllib.request.urlopen("https://ipv4.icanhazip.com").read().decode().strip()
-        wan_ipv6 = urllib.request.urlopen("https://ipv6.icanhazip.com").read().decode().strip()
         print("Endereço IP da WAN em IPv4:", wan_ipv4)
-        print("Endereço IP da WAN em IPv6:", wan_ipv6)
         
+        if response6 == 0:
+            wan_ipv6 = urllib.request.urlopen("https://ipv6.icanhazip.com").read().decode().strip()
+            print("Endereço IP da WAN em IPv6:", wan_ipv6)
         
+
+
         # Crie uma mensagem com as informações do IP
         message = f"Hostname: {hostname}\nEndereço IP da LAN em IPv4: {lan_ipv4}\nEndereço IP da WAN em IPv4: {wan_ipv4}\nEndereço IP da WAN em IPv6: {wan_ipv6}"
 
-        send_to_telegram(message)    
+        send_to_telegram(message)
 
-     
+
         break
     else:
         print("Ping não concluído. Tentando novamente em 5 segundos...")
